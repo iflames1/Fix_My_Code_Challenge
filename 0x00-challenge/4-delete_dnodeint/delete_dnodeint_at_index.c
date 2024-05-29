0495b2a97b5450a592b0efa03bde16b4737ce97d
Flames <isaacomenuche@gmail.com>
2024-05-29 18:57:22 +0100

    delete node

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
@@ -11,43 +12,42 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+    dlistint_t *current;
+    unsigned int p;
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
-	}
-	return (1);
+    if (head == NULL || *head == NULL)
+        return (-1);
+
+    current = *head;
+
+    /* Traverse to the node at the specified index */
+    for (p = 0; current != NULL && p < index; p++)
+        current = current->next;
+
+    /* If the index is out of range */
+    if (current == NULL)
+        return (-1);
+
+    /* If the node to be deleted is the head node */
+    if (current == *head)
+    {
+        *head = current->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+    }
+    else
+    {
+        /* Update the next pointer of the previous node */
+        if (current->prev != NULL)
+            current->prev->next = current->next;
+
+        /* Update the prev pointer of the next node */
+        if (current->next != NULL)
+            current->next->prev = current->prev;
+    }
+
+    /* Free the memory allocated for the current node */
+    free(current);
+
+    return (1);
 }
